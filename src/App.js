6d03068b27b7403e4b76ca1a8c8d65499e0529f4
Bryan Choong <bryanchoong12@gmail.com>
2023-08-11 20:40:02 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import "./assets/css/root.css"
+
+import Homepage from './pages/Homepage';
+import PageA from './pages/PageA';
+import PageB from './pages/PageB';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/page-a" element={<PageA />} />
+        <Route path="/page-b" element={<PageB />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/LogoSpin/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "../../assets/css/logoSpin.css";
+
+import { Logo } from "../../assets/img";
+
+
+export function LogoSpin() {
+    return (
+        <img className="logoImg" src={Logo} alt="logo" />
+    )
+}
