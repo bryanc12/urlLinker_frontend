9a7124e7e02c52401479271c05bae8d91a4b7bfb
Bryan Choong <bryanchoong12@gmail.com>
2024-12-17 01:06:27 +0800

    project init from boilerplate

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import "./assets/css/root.css"
+import "./assets/css/root.css";
 
-import Homepage from './pages/Homepage';
-import PageA from './pages/PageA';
-import PageB from './pages/PageB';
+import Homepage from "./pages/Homepage";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/page-a" element={<PageA />} />
-        <Route path="/page-b" element={<PageB />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,19 +1,10 @@
 import React from "react";
 
-import "../../assets/css/homepage.css"
-
-import { Navbar } from "../../components/Navbar";
-import { LogoSpin } from "../../components/LogoSpin";
-
 export default function Homepage() {
     return (
         <>
-            <Navbar />
-            <div className="homepage">
-                <div className="logoSpin"><LogoSpin /></div>
-                <div className="title">A ReactJS boilerplate.</div>
-                <div className="description">Written by <a className="descriptionLink" href="https://github.com/bryanc12/reactjs-boilerplate" target="_blank" rel="noopener noreferrer">Bryan</a></div>
-            </div>
+            <h1>Homepage</h1>
+            <p>Welcome to the homepage!</p>
         </>
-    )
+    );
 }
